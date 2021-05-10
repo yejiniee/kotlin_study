11397945a492bcc5bf19f014771edd67c1ecc8ba
jimin3263 <dkdldsla0000@naver.com>
2021-05-10 23:02:25 +0900

    feat: add google login

--- Instagram/Howlstagram_f16/app/src/main/java/com/example/howlstagram_f16/LoginActivity.kt
+++ Instagram/Howlstagram_f16/app/src/main/java/com/example/howlstagram_f16/LoginActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +24,64 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+
+        // Google Login 버튼 클릭
+        google_sign_in_button.setOnClickListener{
+            googleLogin()
+        }
+
+        // Configure Google Sign In
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+
+            if (result!!.isSuccess) {
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+
+    }
+
+    /* google login */
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+    }
+    fun firebaseAuthWithGoogle(account:GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // 성공 -> main으로 이동
+                    //moveMainPage(task.result?.user)
+                    val user = auth!!.currentUser
+                    moveMainPage(user)
+                } else {
+                    // 실패 -> 토스트 띄움
+                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
+
+                }
+            }
+    }
+
+    /* signUp or signIn*/
+
     fun signinAndSignup() {
         auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
             ?.addOnCompleteListener {
-            task->
+                    task->
                 if(task.isSuccessful) {
                     //Creating a user account
                     moveMainPage(task.result?.user)
@@ -34,26 +92,24 @@ class LoginActivity : AppCompatActivity() {
                     //Login if you have eaccount
                     signinEmail()
                 }
-
             }
-
     }
 
     fun signinEmail() {
         auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
             ?.addOnCompleteListener {
-            task->
+                    task->
                 if(task.isSuccessful) {
                     //Login
                     moveMainPage(task.result?.user)
                 }else {
                     //Show the error message
-                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG)
+                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
                 }
-
             }
     }
 
+    /* 로그인 성공 -> main으로 이동 */
     fun moveMainPage(user: FirebaseUser?) {
         if(user != null) {
             startActivity(Intent(this, MainActivity::class.java))
